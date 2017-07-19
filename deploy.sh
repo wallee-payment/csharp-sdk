613b63cdbcb0a63a0d51b38d8167428b74c2ad6f
Slobodan Adamovic <sloba@sloba.local>
2017-07-19 13:32:01 +0200

    Add missing -ApiKey attribute in  deploy script.

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 nuget pack src/Customweb.Wallee/Customweb.Wallee.nuspec -Version $WALLEE_SDK_VERSION && \
-nuget push *.nupkg $NUGET_API_KEY -verbosity detailed;
+nuget push *.nupkg -ApiKey $NUGET_API_KEY -Verbosity detailed;
