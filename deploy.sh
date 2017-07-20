33b280892ea014043ffbbb6d215822819476b276
Slobodan Adamovic <sloba@sloba.local>
2017-07-20 13:44:08 +0200

    Display current deploy directory and all its sub-directories

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-ls * -lahr && nuget pack src/Customweb.Wallee/Customweb.Wallee.nuspec -Version $WALLEE_SDK_VERSION -Verbosity detailed && \
+ls */*/* -lahr && nuget pack src/Customweb.Wallee/Customweb.Wallee.nuspec -Version $WALLEE_SDK_VERSION -Verbosity detailed && \
 nuget push *.nupkg -ApiKey $NUGET_API_KEY -Verbosity detailed
