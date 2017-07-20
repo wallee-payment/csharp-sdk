14897c8766c7c1c8b3397757473c4f2299633971
Slobodan Adamovic <sloba@sloba.local>
2017-07-20 13:33:43 +0200

    Add debug info to deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-nuget pack src/Customweb.Wallee/Customweb.Wallee.nuspec -Version $WALLEE_SDK_VERSION && \
+ls * -lahr && nuget pack src/Customweb.Wallee/Customweb.Wallee.nuspec -Version $WALLEE_SDK_VERSION -Verbosity detailed && \
 nuget push *.nupkg -ApiKey $NUGET_API_KEY -Verbosity detailed
