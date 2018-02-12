199853502b8824b7c86b69404299e2a8949d8311
Alan Wales <alan@kostuemli.ch>
2018-02-12 13:15:45 +0100

    Migrated to .net standard

--- src/Customweb.Wallee/Client/ApiClient.cs
+++ src/Customweb.Wallee/Client/ApiClient.cs
@@ -121,7 +121,8 @@ namespace Customweb.Wallee.Client
             // add file parameter, if any
             foreach(var param in fileParams)
             {
-                request.AddFile(param.Value.Name, param.Value.Writer, param.Value.FileName, param.Value.ContentType);
+                // param.Value.Name, param.Value.Writer, param.Value.FileName, param.Value.ContentType
+                request.AddFile(param.Value.Name, param.Value.FileName, param.Value.ContentType);
             }
 
             if (postBody != null) // http body (model or byte[]) parameter
